8085c4e2052fb0f99cdfe06a53a7fbbb277e6d81
kshopov <krasimir.shopov@gmail.com>
2023-11-16 16:41:45 +0200

    Role entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,70 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	public static final String ROLE_ADMIN = "Admin";
+	
+	public static final String ROLE_SALES_PERSON = "Salesperson";
+	
+	public static final String ROLE_EDITOR = "Editor";
+	
+	public static final String ROLE_SHIPPER= "Shipper";
+	
+	public static final String ROLE_ASSISTANT = "Assistant";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 40, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 150, nullable = false)
+	private String description;
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+	public Role() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
