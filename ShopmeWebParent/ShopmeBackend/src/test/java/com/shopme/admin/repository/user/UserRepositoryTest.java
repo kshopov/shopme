84db35d5bb4950f6ec580787d3c653410e4156ec
kshopov <krasimir.shopov@gmail.com>
2023-11-17 16:34:58 +0200

    import removed

--- ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/repository/user/UserRepositoryTest.java
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/repository/user/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
