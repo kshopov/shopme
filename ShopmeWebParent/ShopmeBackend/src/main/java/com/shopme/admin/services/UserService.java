4d47eee5948629ee5f13ccaf4d7551057cfcc7bf
kshopov <krasimir.shopov@gmail.com>
2023-11-23 19:39:06 +0200

    add listRoles and Save methods

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/services/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/services/UserService.java
@@ -4,20 +4,33 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.shopme.admin.repository.user.RoleRepository;
 import com.shopme.admin.repository.user.UserRepository;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.ShopmeUser;
 
 @Service
 public class UserService {
 
 	private final UserRepository userRepository;
+	
+	private final RoleRepository roleRepository;
 
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, RoleRepository roleRepository) {
 		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
 	}
 	
 	public List<ShopmeUser> listAll() {
 		return (List<ShopmeUser>) userRepository.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepository.findAll();
+	}
+	
+	public void save(ShopmeUser user) {
+		userRepository.save(user);
 	}
 
 }
