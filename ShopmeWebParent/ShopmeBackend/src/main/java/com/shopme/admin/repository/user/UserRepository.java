d2861362bdc01ea3cea205fb9540723d5a3b0bc4
kshopov <krasimir.shopov@gmail.com>
2023-11-26 13:59:17 +0200

    add getUserByEmail method

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/repository/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/repository/user/UserRepository.java
@@ -1,9 +1,14 @@
 package com.shopme.admin.repository.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.shopme.common.entity.ShopmeUser;
 
 public interface UserRepository extends CrudRepository<ShopmeUser, Long> {
-
+	
+	@Query("SELECT u FROM users u WHERE u.email = :email")
+	public ShopmeUser getUserByEmail(@Param("email") String email);
+	
 }
