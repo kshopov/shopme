3ef98df88b8514d7f4ce665dfa26d26a12896a80
kshopov <krasimir.shopov@gmail.com>
2023-11-26 13:58:51 +0200

    Create UserRestControler with chekDuplicateEmail method

---
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controllers/UserRestController.java
@@ -0,0 +1,22 @@
+package com.shopme.admin.controllers;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.admin.services.UserService;
+
+@RestController
+public class UserRestController {
+	
+	private final UserService userService;
+	
+	public UserRestController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/users/check_email")
+	public String checkDuplicateEmail(@Param("email") String email) {
+		return userService.isEmailUnique(email) ? "Ok" : "Duplicated";
+	}
+}
