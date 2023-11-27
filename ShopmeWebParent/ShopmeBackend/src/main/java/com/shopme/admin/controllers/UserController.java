e4e403f050afbc4c1730e02c9c36d784dca599fe
kshopov <krasimir.shopov@gmail.com>
2023-11-27 20:15:33 +0200

    delete user method

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controllers/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.shopme.admin.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.exceptions.UserNotFoundException;
 import com.shopme.admin.services.UserService;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.ShopmeUser;
@@ -27,6 +27,8 @@ public class UserController {
 	
 	private static final String PAGE_TITLE_PARAM_VALUE_EDIT_USER = "Edit User with Id: ";
 
+	private static final String PAGE_TITLE_PARAM_VALUE_DELETE_USER = "Deleted User with Id: ";
+	
 	private final UserService userService;
 
 	public UserController(UserService userService) {
@@ -74,10 +76,26 @@ public class UserController {
 			model.addAttribute(PAGE_TITLE_PARAM_TEXT, PAGE_TITLE_PARAM_VALUE_EDIT_USER + id);			
 
 			return "user_form";
-		} catch (Exception ex) {
+		} catch (UserNotFoundException ex) {
 			redirectAttributes.addFlashAttribute(MESSAGE_TEXT, ex.getMessage());
 			return "redirect:/users";
 		}
+	}
+	
+	@GetMapping("/users/delete/{id}")
+	public String deleteUser(@PathVariable(name = "id") Long id,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			ShopmeUser user = userService.get(id);
+			userService.delete(user.getId());
+
+			redirectAttributes.addFlashAttribute(MESSAGE_TEXT, PAGE_TITLE_PARAM_VALUE_DELETE_USER + id);
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute(MESSAGE_TEXT, ex.getMessage());
+		}
+
+		return "redirect:/users";
 	}
 	
 }
