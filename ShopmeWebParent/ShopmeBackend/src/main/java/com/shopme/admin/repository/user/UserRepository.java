827d4c627989474c8044e0598f2b975c402b220d
kshopov <krasimir.shopov@gmail.com>
2023-11-28 22:03:55 +0200

    add updateEnableStatus method

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/repository/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/repository/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.repository.user;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +11,9 @@ public interface UserRepository extends CrudRepository<ShopmeUser, Long> {
 	
 	@Query("SELECT u FROM users u WHERE u.email = :email")
 	public ShopmeUser getUserByEmail(@Param("email") String email);
+	
+	@Query("UPDATE users u SET u.isEnabled = ?2 WHERE u.id = ?1")
+	@Modifying
+	public void updateEnableStatus(Long id, boolean enabled);
 	
 }
