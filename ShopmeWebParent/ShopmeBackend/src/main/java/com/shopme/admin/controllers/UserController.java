2d932b93f63008f3e4f125f59fbe108c05a3e498
kshopov <krasimir.shopov@gmail.com>
2023-12-04 12:21:18 +0200

    implemented save photos functionality to saveUser method

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controllers/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.shopme.admin.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.exceptions.UserNotFoundException;
 import com.shopme.admin.services.UserService;
+import com.shopme.admin.util.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.ShopmeUser;
 
@@ -56,8 +61,25 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(ShopmeUser user, RedirectAttributes redirectAttributes) {
-		userService.save(user);
+	public String saveUser(ShopmeUser user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		
+		if(!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			ShopmeUser savedUser = userService.save(user);
+			
+			String uploadDir = ShopmeUser.USER_PHOTOS_DIR + "/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDirectory(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if(user.getPhotos().isEmpty())
+				user.setPhotos(null);
+			userService.save(user);
+		}
+		
+
 		
 		redirectAttributes.addFlashAttribute(MESSAGE_TEXT, MESSAGE_SUCCESS_SAVE_USER);
 		return "redirect:/users";
